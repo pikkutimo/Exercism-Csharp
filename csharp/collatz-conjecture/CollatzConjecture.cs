c41c5d11c5920a2ec1124b17c1b3db9e21490f9a
Timo Vilen <timo.a.vilen@tuni.fi>
2020-10-22 09:20:54 +0300

    Collatz-conjecture 10/22/2002

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number > 0)
+        {
+            var collatz = 0;
+
+            while (number != 1)
+            {
+                number = number % 2 == 0 ? number /= 2 : 3 * number + 1;
+                ++collatz;
+            }
+
+            return collatz;
+        }
+
+        throw new ArgumentOutOfRangeException("Number is either 0 or negative.");
+    }
+}
